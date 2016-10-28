5da9766018630f498f6274e513955e51f9440077
Max Hallsett <maxhallsett@alberts-mbp.percussion.local>
2016-10-28 16:02:00 -0400

    update

---
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -0,0 +1,7 @@
+(function () {
+    angular.module('meanapp').controller('mvNavBarLogingCtrl', function($scope){
+        $scope.signin = function (username, password) {
+            console.log("I'm not done yet")
+        }
+    })
+})();
